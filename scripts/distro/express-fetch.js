1b719bcea8eb0e14e563b6eedc8694fc352ddd0a
Tobias Hollstein <tobias@git.com>
2026-06-17 09:30:44 +0200

    generated-package.json removed

---
+++ scripts/distro/express-fetch.js
@@ -0,0 +1,17 @@
+import { runServerGenerator } from '../generators/generate-server.js'
+import { runClientGenerator } from '../generators/generate-client.js'
+import { createExpressRouteBody, createExpressRoutesFileContent } from '../generators/adapters/server/express-adapter.js'
+import { createFetchClientFunction, createFetchClientFileContent } from '../generators/adapters/client/fetch-adapter.js'
+
+const expressAdapter = {
+    createRouteBody: createExpressRouteBody,
+    createRoutesFileContent: createExpressRoutesFileContent
+}
+
+const fetchAdapter = {
+    createClientFunction: createFetchClientFunction,
+    createClientFileContent: createFetchClientFileContent
+}
+
+runServerGenerator(expressAdapter)
+runClientGenerator(fetchAdapter)
---
+++ scripts/distro/fetch.js
@@ -0,0 +1,9 @@
+import { runClientGenerator } from '../generators/generate-client.js'
+import { createFetchClientFunction, createFetchClientFileContent } from '../generators/adapters/client/fetch-adapter.js'
+
+const fetchAdapter = {
+    createClientFunction: createFetchClientFunction,
+    createClientFileContent: createFetchClientFileContent
+}
+
+runClientGenerator(fetchAdapter)
